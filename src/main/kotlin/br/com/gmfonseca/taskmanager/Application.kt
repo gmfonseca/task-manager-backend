5c5bd535a2a8ac2dcca52b5a1cbb9d07d2555f9b
Gabriel Fonseca <gmagnofonseca@gmail.com>
2021-09-19 12:28:16 -0300

    Add CORS support

--- src/main/kotlin/br/com/gmfonseca/taskmanager/Application.kt
+++ src/main/kotlin/br/com/gmfonseca/taskmanager/Application.kt
@@ -3,7 +3,9 @@ package br.com.gmfonseca.taskmanager
 import br.com.gmfonseca.taskmanager.routes.registerTaskRoutes
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.json
 import io.ktor.server.netty.EngineMain
 
@@ -14,6 +16,9 @@ fun Application.module() {
     // Negotiating media types between the client and server. For this, it uses the Accept and Content-Type headers.
     install(ContentNegotiation) {
         json() // adding support to json
+    }
+    install(CORS) {
+        anyHost()
     }
 
     registerTaskRoutes()
